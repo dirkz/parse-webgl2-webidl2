ba9925ea670889e1c6d309b408b433e3987fdbde
Dirk Zimmermann <me@dirkz.com>
2022-07-23 15:40:59 +0200

    Try to decode the whole file

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,14 @@
 
 module Main where
 
-import           Prelude                 hiding ( FilePath )
-import           Turtle
+import           Data.Aeson
+import qualified Data.ByteString.Lazy          as B
+import Text.Pretty.Simple (pPrint)
+
+import           Json.Parser
 
 main :: IO ()
-main = printf "Hello, world!\n"
+main = do
+    jsonBS <- B.readFile "data/webgl2.idl.json"
+    let decls = decode jsonBS :: Maybe [Declaration]
+    pPrint decls
